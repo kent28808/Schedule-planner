7fb1920332d853b9a63c6c3ded1678a9f797e1ea
kent28808 <kktanoue@gmail.com>
2020-09-25 22:29:01 -0700

    Local storage function functional

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ $(document).ready(function () {
         console.log(SiblingText)
         localStorage.setItem(ParentId, SiblingText)
     })
-    
-    $("#hour-10 .description").val(localStorage.getItem("hour-10"))  
+
+    //Local Storage for text content
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));  
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));  
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));  
+    $("#hour-1 .description").val(localStorage.getItem("hour-1"));  
+    $("#hour-2 .description").val(localStorage.getItem("hour-2"));  
+    $("#hour-3 .description").val(localStorage.getItem("hour-3"));  
+    $("#hour-4 .description").val(localStorage.getItem("hour-4"));  
+    $("#hour-5 .description").val(localStorage.getItem("hour-5"));    
     
 })
